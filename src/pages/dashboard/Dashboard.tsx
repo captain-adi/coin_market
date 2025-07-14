20f00a181e1b860adfd8d49b3df57512f1796e15
captain-adi <maniramp554@gmail.com>
2025-07-14 16:30:19 +0530

    feat : apply dark and light theme functionality

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,20 +1,25 @@
 import CoinList from '@/components/coinList'
 import HighLights from '@/components/highlights'
+import LanguageDropDown from '@/components/languageDropdown'
+import { useTheme } from '@/components/themeProvider'
 import { Switch } from '@/components/ui/switch'
-import { Link } from 'react-router-dom'
 
 
 function Dashboard() {
+  const {theme,setTheme} = useTheme()
+  const toggleTheme = ()=>{
+    setTheme(theme === "dark" ? 'light' : "dark")
+  }
   return (
     <div>
 {/* top section  */}
-    <div className='container flex justify-between mx-auto items-center px-4 border'>
+    <div className='container flex justify-between mx-auto items-center p-4'>
       <div className='flex flex-col gap-2'>
         <h3 className='text-3xl '>Cryptocurrency Prices by Market Cap</h3>
-        <p className='text-muted-foreground'>The global cryptocurrency market cap today is $3.73 Trillion, a <span className='text-red-600'> 4.5% </span>  change in the last 24 hours. <Link to={"#"}className='underline'>Read more</Link></p>
       </div>
-      <div className='flex gap-2  items-center'>
-        <span className='text-muted-foreground font-bold'>Highlight</span> <Switch className=''/>
+      <div className='flex gap-9 items-center'>
+        <LanguageDropDown/>
+        <span className='text-muted-foreground font-bold flex justify-center items-center gap-3'>{theme === 'dark' ? "Light Mode" : "Dark Mode "}<Switch onClick={toggleTheme} className=''/></span> 
 
       </div>
      
